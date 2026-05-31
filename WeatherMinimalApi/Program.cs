011892ea1a3b1a15d9ac62975710649b9a68bbff
Juliusz Kopciński <juliusz.kopcinski@gmail.com>
2026-05-31 23:19:35 +0200

    zadanie 2 Web API

---
+++ WeatherMinimalApi/Program.cs
@@ -0,0 +1,47 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var windDirections = new[]
+{
+    "północny",
+    "południowy",
+    "wschodni",
+    "zachodni",
+    "północno-wschodni",
+    "północno-zachodni",
+    "południowo-wschodni",
+    "południowo-zachodni"
+};
+
+app.MapGet("/temperature", () =>
+{
+    var temperature = Random.Shared.Next(-20, 41);
+    return Results.Ok(new
+    {
+        temperature,
+        unit = "C"
+    });
+});
+
+app.MapGet("/wind", () =>
+{
+    var direction = windDirections[Random.Shared.Next(windDirections.Length)];
+    return Results.Ok(new
+    {
+        direction
+    });
+});
+
+app.Run();
